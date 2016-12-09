acbb101d5c4a1302de31dc4d32961eafb2142e59
jasonsanders <jas.sanders@gmail.com>
2016-12-08 20:04:07 -0700

    added local db

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 let massive = require('massive');
 
-let connectionString = process.env.PSQL_DB;
-// let connectionString = 'postgres://postgres:DevMountain@localhost/candy';
+let connectionString = process.env.PSQL_DB || 'postgres://postgres:DevMountain@localhost/candy';
 let massiveInstance = massive.connectSync({connectionString});
 module.exports = massiveInstance;
