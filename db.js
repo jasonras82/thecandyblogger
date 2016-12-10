9efb239eabca573631c05357f16a0995221b86c5
jasonsanders <jas.sanders@gmail.com>
2016-12-10 13:54:50 -0700

    added checkout button

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 let massive = require('massive');
 
-let connectionString = process.env.PSQL_DB || 'postgres://postgres:DevMountain@localhost/candy';
+let connectionString = process.env.PSQL_DB;
+// || 'postgres://postgres:DevMountain@localhost/candy';
 let massiveInstance = massive.connectSync({connectionString});
 module.exports = massiveInstance;
